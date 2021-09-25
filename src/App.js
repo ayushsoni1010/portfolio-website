176873e78ebd97839f816e88b745f782a8b0ec50
Ayush Soni <mr.soni1002001@gmail.com>
2021-09-25 22:11:23 +0530

    🚀Feat: Added Header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div></div>;
-}
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './components/sections-component/Header';
+import './styles/index.css';
+import LandingPage from './views/LandingPage/index';
 
-export default App;
+
+export default function App(){
+  return (
+    <div className = "center_view">
+      <Header />
+      <BrowserRouter>
+        <Route path="/" exact component={LandingPage}/>
+      </BrowserRouter>
+    </div>
+  )
+}
